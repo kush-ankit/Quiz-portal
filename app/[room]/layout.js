1a2dde263f47035f17a3f248e7f70eb2a3607d3c
kush-ankit <er.ankitkush@gmail.com>
2024-02-14 23:55:34 +0530

    edit room

--- app/[room]/layout.js
+++ app/[room]/layout.js
@@ -1,8 +1,13 @@
+"use client"
+
+import { useRouter } from "next/navigation"
 export default function RoomLayout({ children, params }) {
+    const router = useRouter();
     return (
         <section className="p-8 w-full h-full flex flex-col gap-4">
-            <div className="p-4 bg-slate-200 flex justify-end w-full">
+            <div className="p-4 bg-slate-200 flex w-full font-semibold justify-between">
                 <span>{params.room}</span>
+                <button onClick={() => router.push(`/${params.room}/add`)}>Add Question</button>
             </div>
             <div>
                 {children}
