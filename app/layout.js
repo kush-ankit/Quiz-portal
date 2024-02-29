ed1da565eefb7b12a45ebdfe975a3ef637883284
kush-ankit <er.ankitkush@gmail.com>
2024-03-01 00:47:22 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./providers";
 import Navbar from "@/components/ui/navbar";
+import { ThemeProvider } from "@/providers/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +15,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <div className="w-full h-screen flex flex-col">
-            <Navbar />
-            <div className="w-full h-full">
-              {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          <AuthProvider>
+            <div className="w-full h-screen flex flex-col">
+              <Navbar />
+              <div className="w-full h-full">
+                {children}
+              </div>
             </div>
-          </div>
-        </AuthProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
